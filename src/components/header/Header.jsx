f0b5603bc70918eacb77c3820afe3a1513d4f4f5
JayChavan-23 <jaychavan2223@gmail.com>
2022-03-10 21:40:20 +0530

    Social Icons done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assests/meee.png'
+import HeaderSocials from './HeaderSocials'
 
 
 const Header = () => {
@@ -12,6 +13,7 @@ const Header = () => {
           <h1>Jay Chavan</h1>
           <h5 className="text-light">Student and Developer</h5>
           <CTA/>
+          <HeaderSocials/>
 
           <div className="me">
             <img src={ME} alt="me" />
